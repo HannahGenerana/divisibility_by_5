b0b2951b4dcff53f4248f94ad2921e60b9e502e5
HannahGenerana <hannahgenerana@gmail.com>
2024-01-20 22:08:26 +0800

    pseudocode

---
+++ divisibility_by_5.py
@@ -0,0 +1,11 @@
+# Exercise 6: Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# pseudocode
+
+# create a list of random number
+
+# checkif the number is divisible by 5
+
+# print all the list of number divisible by 5
+
